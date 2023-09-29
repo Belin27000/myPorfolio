8f740e91a7175815f9f6f5839239dd2df95071d3
Belin27000 <y.lecerf@protonmail.com>
2023-09-29 12:20:02 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Heading title={"Hello"} />
+      </div>
+    </>
+  )
+}
+
+export default App

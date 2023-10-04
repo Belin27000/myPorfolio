2fb26719281ab522b25fdef4405de6da3d955438
Belin27000 <y.lecerf@protonmail.com>
2023-10-04 12:52:07 +0200

    feat: css hover contact button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ const Button = () => {
     }
 
     return (
-        <button onClick={goToContactForm} className="bg-black hover:text-blueSite text-white shadow-3xl dark:bg-blueSite text-2xl text-black px-5 py-1 rounded-full w-60  " >
+        <button onClick={goToContactForm} className="bg-black dark:hover:text-black hover:text-blueSite text-white shadow-3xl dark:bg-blueSite text-2xl text-black px-5 py-1 rounded-full w-60  " >
             Prendre contact
         </button>
     );
